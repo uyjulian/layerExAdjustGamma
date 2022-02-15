9cd4c69298541154b0767b11033e292b6a3be414
uyjulian <uyjulian@gmail.com>
2022-02-15 16:16:25 -0600

    Sync tvpgl.h definitions

--- tvpgl.h
+++ tvpgl.h
@@ -8,9 +8,11 @@
 #define TVPAdjustGamma_a TVPAdjustGamma_a_c
 #define TVPUninitGammaAdjustTempData TVPUninitGammaAdjustTempData_c
 
+#define TVP_INLINE_FUNC inline
 #define TVP_GL_FUNCNAME(funcname) funcname
 #define TVP_GL_FUNC_DECL(rettype, funcname, arg)  rettype TVP_GL_FUNCNAME(funcname) arg
 #define TVP_GL_FUNC_STATIC_DECL(rettype, funcname, arg)  static rettype TVP_GL_FUNCNAME(funcname) arg
+#define TVP_GL_FUNC_INLINE_DECL(rettype, funcname, arg)  static rettype TVP_INLINE_FUNC TVP_GL_FUNCNAME(funcname) arg
 #define TVP_GL_FUNC_EXTERN_DECL(rettype, funcname, arg)  extern rettype TVP_GL_FUNCNAME(funcname) arg
 #define TVP_GL_FUNC_PTR_EXTERN_DECL TVP_GL_FUNC_EXTERN_DECL
 
